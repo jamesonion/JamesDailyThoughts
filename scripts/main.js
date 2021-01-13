c1b9d80e16b237ea5476821112d7f1d58ff0a1b6
jamesonion <jamesqnguyen1@ufl.edu>
2021-01-12 23:02:03 -0500

    Accordian
    - added entry for 1/12/2021
    ToDO:
    - make accordion open on default
    - update AboutMe
    - update Contact Me

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,19 @@ toggler.addEventListener("click", (e) => {
   e.target.classList.toggle("fa-times")
   navigation.classList.toggle("menu-close")
 })
+
+var accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  }
+  else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+});
+}

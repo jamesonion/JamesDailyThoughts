2850d29f0f446c5576d233d772d25fc873c2764b
James Nguyen <jamesqnguyen1@ufl.edu>
2021-02-01 23:35:09 -0500

    Revamped the accordion slides
    - added two new entries
    - added colors to font awesome icons
    - added font awesome icons next to accordion entries

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//js script
+
 var toggler = document.querySelector(".menu-toggle");
 var navigation = document.querySelector("navigation");
 
@@ -5,20 +7,35 @@ toggler.addEventListener("click", (e) => {
   e.target.classList.toggle("fa-bars")
   e.target.classList.toggle("fa-times")
   navigation.classList.toggle("menu-close")
-})
-
-var accordion = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-  var panel = this.nextElementSibling;
-  if (panel.style.maxHeight) {
-    panel.style.maxHeight = null;
-  }
-  else {
-    panel.style.maxHeight = panel.scrollHeight + "px";
-  }
 });
-}
+
+jQuery(document).ready(function($) {
+  // Hide all panels
+  var panels = $('.accordion .panel').hide();
+
+  // Have first panel show
+  panels.first().show();
+
+  $('.accordion > button > div').click(function() {
+    var $this = $(this);
+    let visible = false;
+
+    if ($this.parent().next().is(":visible"))
+      visible = true;
+
+    // Slide up all other panels
+    panels.slideUp();
+
+    // Set all to closed book
+     $('.accordion button div').find('i').removeClass('fas fa-book-open');
+     $('.accordion button div').find('i').addClass('fas fa-book')
+
+     // set page being opened to open book
+    if (!visible) {
+      $this.parent().next().slideDown();
+      $(this).find('i').addClass("fas fa-book-open");
+    }
+    return false;
+  });
+
+});
